0232d92a3ca8d151e478f09f9b03ede6e3301e20
Pablom25 <pablomaych@icloud.com>
2025-06-12 17:20:45 +0200

    feat: devuelve set proyectos confirmados y sin confirmar, y diccionario de longitud ocupada/localización por proyectos confirmados

--- procesarDatos.py
+++ procesarDatos.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def preprocesar_datos(proyectos: pd.DataFrame, periodos: pd.DataFrame, muelles: pd.DataFrame, fecha_inicial: pd.Timestamp) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, list]:
+def preprocesar_datos(proyectos: pd.DataFrame, periodos: pd.DataFrame, muelles: pd.DataFrame, fecha_inicial: pd.Timestamp) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, list, set, set, dict]:
     """Preprocesa los datos de proyectos, periodos y muelles para su uso en la optimización.
 
     Parameters
@@ -24,6 +24,12 @@ def preprocesar_datos(proyectos: pd.DataFrame, periodos: pd.DataFrame, muelles: 
         DataFrame con las dimensiones de los muelles.
     dias : list
         Lista de días desde la fecha inicial hasta la fecha final de los periodos.
+    set_confirmados : set
+        Set de proyectos confirmados (no a optimizar).
+    set_sinConfirmar : set
+        Set de proyectos sin confirmar (a optimizar).
+    longitudes_confirmados : dict
+        Diccionario con la longitud total de barcos confirmados por ubicación.
     """
 
     # Convertir fechas a integer
@@ -33,4 +39,11 @@ def preprocesar_datos(proyectos: pd.DataFrame, periodos: pd.DataFrame, muelles: 
     # Crear una lista de días
     dias = list(range(periodos['fecha_inicio'].min(), periodos['fecha_fin'].max()+1))
 
-    return proyectos, periodos, muelles, dias
+    # Crear set de proyectos confirmados y sin confirmar
+    set_sinConfirmar = set(proyectos[proyectos['proyecto_a_optimizar']].index)
+    set_confirmados = set(proyectos[~proyectos['proyecto_a_optimizar']].index)
+
+    # Crear diccionario de longitud total de barcos confirmados por ubicación
+    longitudes_confirmados = proyectos[~proyectos['proyecto_a_optimizar']].groupby('nombre_area')['eslora'].sum().to_dict()
+
+    return proyectos, periodos, muelles, dias, set_confirmados, set_sinConfirmar, longitudes_confirmados
