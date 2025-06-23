b35ac1c02b7e340c2706babc7a6743f21b796bbd
Pablom25 <pablomaych@icloud.com>
2025-06-23 13:24:41 +0200

    perf: leer datos y procesar datos vectorizado

--- leerDatos.py
+++ leerDatos.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import json
 
 def leer_datos(path: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.Timestamp, int, int, dict, dict]:
@@ -30,10 +31,10 @@ def leer_datos(path: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.
     """    
 
     with open(path, 'r') as f:
-        data = json.load(f)
+        json_data = json.load(f)
 
         # Df calles
-        calles = pd.DataFrame(data['astican_info']['calles'])
+        calles = pd.DataFrame(json_data['astican_info']['calles'])
         calles.set_index("nombre", inplace=True)
 
         # Df muelles
@@ -42,7 +43,7 @@ def leer_datos(path: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.
             'nombre': []
         }
 
-        for m in data['astican_info']['muelles']:
+        for m in json_data['astican_info']['muelles']:
             if not m['nombre'].startswith('MANIOBRA'):
                 muelles_data['longitud'].append(m['longitud'])
                 muelles_data['nombre'].append(m['nombre'])
@@ -51,213 +52,39 @@ def leer_datos(path: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.
         muelles.set_index('nombre', inplace=True)
 
         # Dict syncrolift
-        syncrolift_dims = data['astican_info']['syncrolift']
+        syncrolift_dims = json_data['astican_info']['syncrolift']
 
         # Dict optimizador_params
-        optimizador_params = data["config"]
+        optimizador_params = json_data["config"]
 
         # Fecha inicial
-        fecha_inicial = pd.to_datetime(data["query_info"]["from_date"])
+        fecha_inicial = pd.to_datetime(json_data["query_info"]["from_date"])
 
-        # Df proyectos y periodos, dict movimientos anteriores
-        proyectos_data = {
-            'eslora': [],
-            'manga': [],
-            'proyecto_id': [],
-            'facturacion': [],
-            'proyecto_a_optimizar': []
-        }
+        # List of projects to optimize
+        proyectos_a_optimizar = sorted(json_data["projects_to_optimize"])
 
-        periodos_data = {
-            'tipo_desc': [],
-            'fecha_inicio': [],
-            'fecha_fin': [],
-            'nombre_area': [],
-            'proyecto_id': [],
-            'periodo_id': [] 
-        }
-        
-        movimientos_anteriores = {}
+        # Projects info
+        rows_info = []
+        for proyecto_id in json_data["projects_info"]:
+            info = json_data["projects_info"][proyecto_id]["info"]
+            info["proyecto_id"] = proyecto_id
+            info["proyecto_a_optimizar"] = proyecto_id in proyectos_a_optimizar
+            rows_info.append(info)
+        proyectos = pd.DataFrame(rows_info).replace(np.nan, None)  # fix facturacion none
 
-        for p in data['projects_info'].keys():
-
-            # Si es un proyecto a optimizar guardar datos proyecto
-            if p in data["projects_to_optimize"]:
-                proyectos_data['eslora'].append(data['projects_info'][p]['info']['eslora'])
-                proyectos_data['manga'].append(data['projects_info'][p]['info']['manga'])
-                proyectos_data['proyecto_id'].append(p)
-                proyectos_data['facturacion'].append(data['projects_info'][p]['info']['facturacion'])
-                proyectos_data['proyecto_a_optimizar'].append(True)
-
-                n = 0
-
-                # Comprobar si el primer periodo empieza antes de la fecha inicial y termina después, si sí separar en dos periodos
-                if pd.to_datetime(data['projects_info'][p]['periodos'][0]['fecha_inicio']) < fecha_inicial:
-                    if pd.to_datetime(data['projects_info'][p]['periodos'][0]['fecha_fin']) >= fecha_inicial:
-                        periodos_data['tipo_desc'].append(data['projects_info'][p]['periodos'][0]['tipo_desc'])
-                        periodos_data['fecha_inicio'].append(data['projects_info'][p]['periodos'][0]['fecha_inicio'])
-                        periodos_data['fecha_fin'].append(fecha_inicial - pd.Timedelta(days=1))
-                        periodos_data['nombre_area'].append(data['projects_info'][p]['periodos'][0]['nombre_area'])
-                        periodos_data['proyecto_id'].append(p)
-                        periodos_data['periodo_id'].append(n) 
-
-                        n += 1
-                        periodos_data['tipo_desc'].append(data['projects_info'][p]['periodos'][0]['tipo_desc'])
-                        periodos_data['fecha_inicio'].append(fecha_inicial)
-                        periodos_data['fecha_fin'].append(data['projects_info'][p]['periodos'][0]['fecha_fin'])
-                        periodos_data['nombre_area'].append("SIN UBICACION ASIGNADA")
-                        periodos_data['proyecto_id'].append(p)
-                        periodos_data['periodo_id'].append(n) 
-                    
-                    # Si termina antes de fecha_inicial dejar su nombre_area
-                    else:
-                        periodos_data['tipo_desc'].append(data['projects_info'][p]['periodos'][0]['tipo_desc'])
-                        periodos_data['fecha_inicio'].append(data['projects_info'][p]['periodos'][0]['fecha_inicio'])
-                        periodos_data['fecha_fin'].append(data['projects_info'][p]['periodos'][0]['fecha_fin'])
-                        periodos_data['nombre_area'].append(data['projects_info'][p]['periodos'][0]['nombre_area'])
-                        periodos_data['proyecto_id'].append(p)
-                        periodos_data['periodo_id'].append(n)
-
-                # Si no registrar los datos del primer periodo
-                else:
-                    periodos_data['tipo_desc'].append(data['projects_info'][p]['periodos'][0]['tipo_desc'])
-                    periodos_data['fecha_inicio'].append(data['projects_info'][p]['periodos'][0]['fecha_inicio'])
-                    periodos_data['fecha_fin'].append(data['projects_info'][p]['periodos'][0]['fecha_fin'])
-                    periodos_data['nombre_area'].append("SIN UBICACION ASIGNADA")
-                    periodos_data['proyecto_id'].append(p)
-                    periodos_data['periodo_id'].append(n)
-
-                # Para el resto de periodos, mirar primero si son del mismo tipo que el anterior
-                for p_k in data['projects_info'][p]['periodos'][1:]:
-                    if periodos_data['proyecto_id'][-1] == p and periodos_data['tipo_desc'][-1] == p_k['tipo_desc']:
-                        # Comprobar si la fecha fin del periodo previo es anterior a fecha_inicial
-                        if pd.to_datetime(periodos_data['fecha_fin'][-1]) < fecha_inicial:
-                            # Comprobar si la fecha fin del periodo nuevo es posterior a fecha_inicial
-                            if pd.to_datetime(p_k['fecha_fin']) >= fecha_inicial:
-                                # Comprobar si estan en el mismo area
-                                if periodos_data['nombre_area'][-1] == p_k['nombre_area']:
-                                    # Si 2 periodos consecutivos + mismo tipo + fecha fin anterior < fecha_inicial + fecha fin nuevo >= fecha inicial + mismo area --> extender anterior hasta fecha inicial -1, y crear uno nuevo desde fecha inicial hasta la nueva fecha fin
-                                    periodos_data['fecha_fin'][-1] = fecha_inicial - pd.Timedelta(days=1)
-                                    
-                                    n += 1
-                                    periodos_data['tipo_desc'].append(periodos_data['tipo_desc'][-1])
-                                    periodos_data['fecha_inicio'].append(fecha_inicial)
-                                    periodos_data['fecha_fin'].append(p_k['fecha_fin'])
-                                    periodos_data['nombre_area'].append("SIN UBICACION ASIGNADA")
-                                    periodos_data['proyecto_id'].append(p)
-                                    periodos_data['periodo_id'].append(n)
-                                else:
-                                    # Si 2 periodos consecutivos + mismo tipo + fecha fin anterior < fecha_inicial + fecha fin nuevo >= fecha inicial + distinto area --> crear nuevo periodo partido en dos por fecha_inicial, añadir 1 a movimientos_anteriores de ese proyecto
-                                    n += 1
-                                    periodos_data['tipo_desc'].append(periodos_data['tipo_desc'][-1])
-                                    periodos_data['fecha_inicio'].append(p_k['fecha_inicio'])
-                                    periodos_data['fecha_fin'].append(fecha_inicial - pd.Timedelta(days=1))
-                                    periodos_data['nombre_area'].append(p_k["nombre_area"])
-                                    periodos_data['proyecto_id'].append(p)
-                                    periodos_data['periodo_id'].append(n)
-
-                                    n += 1
-                                    periodos_data['tipo_desc'].append(periodos_data['tipo_desc'][-1])
-                                    periodos_data['fecha_inicio'].append(fecha_inicial)
-                                    periodos_data['fecha_fin'].append(p_k['fecha_fin'])
-                                    periodos_data['nombre_area'].append("SIN UBICACION ASIGNADA")
-                                    periodos_data['proyecto_id'].append(p)
-                                    periodos_data['periodo_id'].append(n)
-
-                                    movimientos_anteriores[p] = movimientos_anteriores.get(p,0) + 1
-                            
-                            elif periodos_data['nombre_area'][-1] == p_k['nombre_area']:
-                                # Si 2 periodos consecutivos + mismo tipo + fecha fin anterior < fecha_inicial + fecha fin nuevo < fecha inicial + mismo area --> extender periodo anterior
-                                periodos_data['fecha_fin'][-1] = fecha_inicial - pd.Timedelta(days=1)
-
-                            else:
-                                # Si 2 periodos consecutivos + mismo tipo + fecha fin anterior < fecha_inicial + fecha fin nuevo < fecha inicial + distinto area area --> crear entradas nuevo periodo, añadir 1 a movimientos_anteriores de ese proyecto
-                                n += 1
-                                periodos_data['tipo_desc'].append(periodos_data['tipo_desc'][-1])
-                                periodos_data['fecha_inicio'].append(p_k['fecha_inicio'])
-                                periodos_data['fecha_fin'].append(p_k['fecha_fin'])
-                                periodos_data['nombre_area'].append(p_k["nombre_area"])
-                                periodos_data['proyecto_id'].append(p)
-                                periodos_data['periodo_id'].append(n)
-
-                                movimientos_anteriores[p] = movimientos_anteriores.get(p,0) + 1
-
-                        # Si el periodo anterior termina después de fecha_inicial, extenderlo   
-                        else:
-                            periodos_data['fecha_fin'][-1] = p_k['fecha_fin']
-                    
-                    # Si no, crear entradas nuevo periodo
-                    # Comprobar si empieza antes y termina después de fecha_inicial, si sí dividir
-                    elif pd.to_datetime(p_k['fecha_inicio']) < fecha_inicial and pd.to_datetime(p_k['fecha_fin']) >= fecha_inicial:
-                        n += 1
-                        periodos_data['tipo_desc'].append(p_k['tipo_desc'])
-                        periodos_data['fecha_inicio'].append(p_k['fecha_inicio'])
-                        periodos_data['fecha_fin'].append(fecha_inicial - pd.Timedelta(days=1))
-                        periodos_data['nombre_area'].append(p_k['nombre_area'])
-                        periodos_data['proyecto_id'].append(p)
-                        periodos_data['periodo_id'].append(n)
-
-                        n += 1
-                        periodos_data['tipo_desc'].append(p_k['tipo_desc'])
-                        periodos_data['fecha_inicio'].append(fecha_inicial)
-                        periodos_data['fecha_fin'].append(p_k['fecha_fin'])
-                        periodos_data['nombre_area'].append("SIN UBICACION ASIGNADA")
-                        periodos_data['proyecto_id'].append(p)
-                        periodos_data['periodo_id'].append(n)
-
-                    # Si no crear entradas periodo
-                    else:
-                        n += 1
-                        periodos_data['tipo_desc'].append(p_k['tipo_desc'])
-                        periodos_data['fecha_inicio'].append(p_k['fecha_inicio'])
-                        periodos_data['fecha_fin'].append(p_k['fecha_fin'])
-                        periodos_data['nombre_area'].append("SIN UBICACION ASIGNADA")
-                        periodos_data['proyecto_id'].append(p)
-                        periodos_data['periodo_id'].append(n)
-
-            # Si no es un proyecto a optimizar, pero tiene alguna ubicación asignada, crear entradas del proyecto
-            elif any(p_k['nombre_area']!= 'SIN UBICACION ASIGNADA' for p_k in data['projects_info'][p]['periodos']):
-                proyectos_data['eslora'].append(data['projects_info'][p]['info']['eslora'])
-                proyectos_data['manga'].append(data['projects_info'][p]['info']['manga'])
-                proyectos_data['proyecto_id'].append(p)
-                proyectos_data['facturacion'].append(data['projects_info'][p]['info']['facturacion'])
-                proyectos_data['proyecto_a_optimizar'].append(False)
-
-                n = 0
-
-                # Registrar los datos del primer periodo
-                periodos_data['tipo_desc'].append(data['projects_info'][p]['periodos'][0]['tipo_desc'])
-                periodos_data['fecha_inicio'].append(data['projects_info'][p]['periodos'][0]['fecha_inicio'])
-                periodos_data['fecha_fin'].append(data['projects_info'][p]['periodos'][0]['fecha_fin'])
-                periodos_data['nombre_area'].append(data['projects_info'][p]['periodos'][0]['nombre_area'])
-                periodos_data['proyecto_id'].append(p)
-                periodos_data['periodo_id'].append(n)
-
-                # Para el resto de periodos, mirar primero si están en la misma ubicacion que el anterior
-                for p_k in data['projects_info'][p]['periodos'][1:]:
-                    # Si sí, unificarlos
-                    if periodos_data['proyecto_id'][-1] == p and periodos_data['nombre_area'][-1] == p_k['nombre_area']:
-                        periodos_data['fecha_fin'][-1] = p_k['fecha_fin']
-                    # Si no, crear entradas nuevo periodo            
-                    else:
-                        n += 1
-                        periodos_data['tipo_desc'].append(p_k['tipo_desc'])
-                        periodos_data['fecha_inicio'].append(p_k['fecha_inicio'])
-                        periodos_data['fecha_fin'].append(p_k['fecha_fin'])
-                        periodos_data['nombre_area'].append(p_k['nombre_area'])
-                        periodos_data['proyecto_id'].append(p)
-                        periodos_data['periodo_id'].append(n)
+        # Repair periods info
+        rows_periodos = []
+        for proyecto_id in json_data["projects_info"]:
+            for periodo in json_data["projects_info"][proyecto_id]["periodos"]:
+                periodo["proyecto_id"] = proyecto_id
+                rows_periodos.append(periodo)
+        periodos = pd.DataFrame(rows_periodos)
     
-        proyectos = pd.DataFrame(proyectos_data)
         proyectos.set_index('proyecto_id', inplace=True)
-
-        periodos = pd.DataFrame(periodos_data)
         periodos['fecha_inicio'] = pd.to_datetime(periodos['fecha_inicio'])
         periodos['fecha_fin'] = pd.to_datetime(periodos['fecha_fin'])
-        periodos['id_proyecto_reparacion'] = periodos['proyecto_id'] + '_' + periodos['periodo_id'].astype(str)
-        periodos.set_index('id_proyecto_reparacion', inplace=True)
 
-    return proyectos, periodos, muelles, calles, fecha_inicial, syncrolift_dims, optimizador_params, movimientos_anteriores
+    return proyectos, periodos, muelles, calles, fecha_inicial, syncrolift_dims, optimizador_params
 
 
 def leer_parametros(path: str) -> dict:
--- procesarDatos.py
+++ procesarDatos.py
@@ -32,16 +32,59 @@ def preprocesar_datos(proyectos: pd.DataFrame, periodos: pd.DataFrame, muelles: 
         DataFrame con las dimensiones de los muelles y de las calles.
     dias : list
         Lista de días desde la fecha inicial hasta la fecha final de los periodos.
+    optimizador_params : dict
+        Diccionario con los parámetros para el optimizador
     """
 
     # Convertir fechas a integer
     periodos['fecha_inicio'] = (periodos['fecha_inicio'] - fecha_inicial).dt.days
     periodos['fecha_fin'] = (periodos['fecha_fin'] - fecha_inicial).dt.days
 
-    # Facturacion diaria
+    # Unificar periodos consecutivos que tienen el mismo nombre area y tipo_desc
+    periodos = periodos.sort_values(['proyecto_id', 'fecha_inicio'])
+    cambio = (
+        (periodos['tipo_desc'] != periodos.groupby('proyecto_id')['tipo_desc'].shift()) |
+        (periodos['nombre_area'] != periodos.groupby('proyecto_id')['nombre_area'].shift()) |
+        (periodos['fecha_inicio'] != periodos.groupby('proyecto_id')['fecha_fin'].shift() + 1)
+    ).astype(int)
+    periodos['grupo'] = cambio.groupby(periodos['proyecto_id']).cumsum()
+    periodos = (periodos.groupby(['proyecto_id', 'grupo'], as_index=False).agg({
+        'fecha_inicio': 'first',
+        'fecha_fin': 'last',
+        'tipo_desc': 'first',
+        'nombre_area': 'first',
+        'proyecto_id': 'first',
+    }))
 
+    periodos.drop(columns=['grupo'], inplace=True)
+    
+    # Facturacion diaria
     duraciones_proyectos = periodos.groupby('proyecto_id').agg(ultimo_dia = ('fecha_fin', 'max'), primer_dia =  ('fecha_inicio', 'min'))
     proyectos['facturacion_diaria'] = proyectos['facturacion']/(proyectos.index.map(duraciones_proyectos['ultimo_dia']) - proyectos.index.map(duraciones_proyectos['primer_dia']))
+
+    # Dividir periodos a optimizar que empiezan antes de fecha_inicial y terminan después
+    cruzan_fecha_inicial = periodos[(periodos['fecha_inicio']<0) & (periodos['fecha_fin']>=0)].copy()
+
+    periodos_anteriores = cruzan_fecha_inicial.copy()
+    periodos_anteriores['fecha_fin'] = -1
+
+    periodos_posteriores = cruzan_fecha_inicial.copy()
+    periodos_posteriores['fecha_inicio'] = 0
+
+    periodos_restantes = periodos[~((periodos['fecha_inicio']<0) & (periodos['fecha_fin']>=0))].copy()
+
+    periodos = pd.concat([periodos_anteriores, periodos_posteriores, periodos_restantes], ignore_index=True)
+    periodos.sort_values(by=['proyecto_id', 'fecha_inicio'], inplace=True)
+
+    # Periodos añadir periodo_id y el index
+    periodos['periodo_id'] = periodos.groupby('proyecto_id').cumcount()
+    periodos['id_proyecto_reparacion'] = periodos['proyecto_id'] + '_' + periodos['periodo_id'].astype(str)
+    periodos.set_index('id_proyecto_reparacion', inplace=True)
+
+    # Cambiar ubicacion periodos a optimizar después de fecha_inicial a "SIN UBICACION ASIGNADA"
+    periodos['proyecto_a_optimizar'] = periodos['proyecto_id'].map(proyectos['proyecto_a_optimizar'])
+    periodos.loc[(periodos['proyecto_a_optimizar'] == True) & (periodos['fecha_inicio'] >= 0), 'nombre_area'] = "SIN UBICACION ASIGNADA"
+    periodos.drop(columns=['proyecto_a_optimizar'], inplace=True)
 
     # Crear una lista de días
     dias = list(range(periodos['fecha_inicio'].min(), periodos['fecha_fin'].max()+1))
